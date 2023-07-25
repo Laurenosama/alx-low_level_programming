19d8372a9261f275ba6031dd75e483c105c66363
Lauren <laurenosama5@gmail.com>
2023-07-24 17:25:23 -0700

    m

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,16 @@
 /**
  * _strlen * returns the length of a string
  *
- * @s: string parameter input 
+ * @s: string parameter input
  *
  * Returns: length of string
  */
 int _strlen(char *s);
 {
 	int counter;
+
 	for (counter = 0; *s != '\0'; s++)
 		++counter;
+
 	return (counter);
 }
