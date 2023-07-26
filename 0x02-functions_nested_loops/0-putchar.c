3d3d889ca89f13c86d48766d651a88045700d660
Lauren <laurenosama5@gmail.com>
2023-07-26 11:48:48 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
-#include <unistd.h>
-#include <stdio.h>
-#include"main.h"
+#include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char str[] = "_putchar";
+	int ch;
+	for (ch = 0; ch < 8; ch++)
+	{
+		_putchar(str[ch]);
+		_putchar('\n');
+	}
 	return (0);
 }
+
