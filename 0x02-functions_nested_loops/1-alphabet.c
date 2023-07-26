97751ab06cf0336eb4afd08d804407e536265c4c
Lauren <laurenosama5@gmail.com>
2023-07-26 14:59:39 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 /**
  * print_alpabet - utilizes on the _putchar function to print
  *                the alphabet a - z
+ * Return: void                                
  */
 void print_alphabet(void)
 {
